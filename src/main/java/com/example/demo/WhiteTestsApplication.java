aaf81aae02fb7d2614fe41660693067e07d34308
achrefbenabdallah <achref.ben-abdallah@tek-up.tn>
2021-05-03 23:57:49 +0100

    first commit

---
+++ src/main/java/com/example/demo/WhiteTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiteTestsApplication.class, args);
+	}
+
+}
